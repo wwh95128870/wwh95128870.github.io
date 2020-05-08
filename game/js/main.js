e9cd163a8978268a92cdb45706fecc65f9b88825
Jackwu_pccw <jack.wh.wu@pccw.com>
2020-05-08 18:25:17 +0800

    update

--- game/js/main.js
+++ game/js/main.js
@@ -1,14 +1,14 @@
 var clicks = 0;
 var lastClick = [0, 0];
 var inputLineArray = [];
-var blockArray ;
-    //x1,y1,x2,y2,blockType 0=cut,touchCount
- 
+var levelBlockArray;
+var blockArray;
+//x1,y1,x2,y2,blockType 0=cut,touchCount
 var pin;
 var gamelose = 0;
 var score = 0;
-var maxScore = 2;
-
+var maxScore;
+var currentLevel = 0;
 
 var defaultPenColor = "#00f";
 var defaultPenColorLose = "#FFA500";
@@ -20,27 +20,29 @@ var ctx;
 var img_pin_cyan;
 var img_pin_red;
 
-function init(c,b,p) {
+function init(c, b, p, m) {
     ctx = c;
     blockArray = b;
-    pin = p
+    levelBlockArray = b;
+    pin = p;
+    maxScore = m;
+    inputLineArray = [];
+    lastClick = [0, 0];
+    clicks = 0;
     document.getElementById('canvas').addEventListener('click', drawLine, false);
     document.getElementById('canvas').addEventListener('ontouchstart', drawLine, false);
     img_pin_cyan = document.getElementById('pin_cyan');
     img_pin_red = document.getElementById('pin_red');
-    drawBlock(ctx, 7);
-    drawPin();
+
+    
+    redraw();
 }
 
 function restart() {
     clicks = 0;
     lastClick = [0, 0];
     inputLineArray = [];
-    blockArray = [
-        //x1,y1,x2,y2,blockType 0=cut,touchCount
-        [10, 20, 500, 700, 0, 0],
-        [500, 10, 1, 300, 1, 0],
-    ]; 
+    blockArray = levelBlockArray;
     gamelose = 0;
     score = 0;
     redraw()
@@ -79,17 +81,17 @@ function lineIntersect(x1, y1, x2, y2, x3, y3, x4, y4) {
     return true;
 }
 
-function intersects(a,b,c,d,p,q,r,s) {
+function intersects(a, b, c, d, p, q, r, s) {
     var det, gamma, lambda;
     det = (c - a) * (s - q) - (r - p) * (d - b);
     if (det === 0) {
-      return false;
+        return false;
     } else {
-      lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
-      gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
-      return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+        lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+        gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+        return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
     }
-  };
+};
 
 
 function drawBlock(canvas, width) {
@@ -104,8 +106,8 @@ function drawBlock(canvas, width) {
                 canvas.strokeStyle = "#000";
             } else if (blockArray[i][5] == 1) {//touch once
                 canvas.strokeStyle = "#0f0";
-                if(!gamelose){score +=1;}
-                    
+                if (!gamelose) { score += 1; }
+
             } else {
                 canvas.strokeStyle = "#f00"; //touch more than once
                 score = 0;
@@ -144,26 +146,26 @@ function drawLines(context) {
         context.beginPath();
         context.moveTo(inputLineArray[i][0], inputLineArray[i][1]);
         context.lineTo(inputLineArray[i][2], inputLineArray[i][3], defaultPenWidth);
-        context.strokeStyle = gamelose? defaultPenColorLose : defaultPenColor;
+        context.strokeStyle = gamelose ? defaultPenColorLose : defaultPenColor;
         context.stroke();
     }
 }
 
-function drawPin(){
-    for(var i = 0 ; i < pin.length ; i++){
-        if(pin[i][2] == 0){
-            ctx.drawImage(img_pin_cyan, pin[i][0]-20, pin[i][1]-20,40,40);
-        }else{
-            ctx.drawImage(img_pin_red, pin[i][0]-20, pin[i][1]-20,40,40);
+function drawPin() {
+    for (var i = 0; i < pin.length; i++) {
+        if (pin[i][2] == 0) {
+            ctx.drawImage(img_pin_cyan, pin[i][0] - 20, pin[i][1] - 20, 40, 40);
+        } else {
+            ctx.drawImage(img_pin_red, pin[i][0] - 20, pin[i][1] - 20, 40, 40);
         }
-        
+
     }
 }
 
 function drawLine(e) {
     context = this.getContext('2d');
-    x = getCursorPosition(e)[0] * 1000 / document.getElementById("canvas").clientWidth - this.offsetLeft;
-    y = getCursorPosition(e)[1] * 1000 / document.getElementById("canvas").clientHeight - this.offsetTop;
+    x = ((getCursorPosition(e)[0]- this.offsetLeft) * 1000 / document.getElementById("canvas").clientWidth) - this.offsetLeft/1000;
+    y = ((getCursorPosition(e)[1]- this.offsetTop) * 1000 / document.getElementById("canvas").clientHeight) - this.offsetTop/1000;
     x = x.toFixed(0);
     y = y.toFixed(0);
 
@@ -174,14 +176,14 @@ function drawLine(e) {
         context.moveTo(lastClick[0], lastClick[1]);
         context.lineTo(x, y, defaultPenWidth);
         inputLineArray.push([lastClick[0], lastClick[1], x, y])
-        context.strokeStyle = gamelose? defaultPenColorLose : defaultPenColor;
+        context.strokeStyle = gamelose ? defaultPenColorLose : defaultPenColor;
         context.stroke();
         redraw()
     }
     console.log(inputLineArray);
 
     lastClick = [x, y];
-
+    levelChangeController();
 
 };
 
@@ -215,6 +217,18 @@ function checkSelfCollision(inputLineArray) {
 }
 
 
+function levelChangeController() {
+    var gameWin = score == maxScore
+    if (gameWin) {
+        currentLevel++;
+        setTimeout(function () {
+            alert("you win, go to next level");
+            init(ctx, gameLevel[currentLevel].blockArray, gameLevel[currentLevel].pin, gameLevel[currentLevel].maxScore);
+
+        }, 100);
+
+    }
+}
 
 
 function redraw() {
@@ -227,10 +241,13 @@ function redraw() {
     drawPin();
     drawLines(ctx);
 
+
     document.getElementById('score').innerText = score;
-    if(score == maxScore){
+    if (score == maxScore) {
         document.getElementById('score').innerText = "you win";
     }
+
+
 }
 
 function undo() {
